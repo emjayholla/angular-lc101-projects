fd0ce046d1bb79811b314ddb9667f1e805b4e219
Maggie Hollander <maggiehollander@Maggies-MacBook-Air.local>
2023-03-08 21:00:30 -0600

    part 1 of exercises done.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'The Cutest Puppies';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://petlandstl.com/wp-content/uploads/2022/11/2573693_800.jpg';
+  image3 = 'https://i.pinimg.com/originals/c2/94/c9/c294c91a09f20d65bc2fc822e47adb89.png';
 
   constructor() { }
 
